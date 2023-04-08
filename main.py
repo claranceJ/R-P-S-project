405fda250c7a820d74d96b9580ae97bcafdd013b
Jacktone Clarance <77950647+claranceJ@users.noreply.github.com>
2023-04-08 16:54:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from getpass import getpass as input
+
+# welcome the user
+print("Welcome to Rock, Paper, Scissors")
+print()
+print("Pick R, P, S")
+print()
+# assign player 1 and 2
+player_1=input("player1, pick: ")
+player_2=input("player2, pick: ")
+# conditions to find the answer
+if player_1=="R" and player_2=="R":
+  print("Rock and Rock, It's a draw")
+elif player_1=="R" and player_2=="P":
+  print("Paper covers Rock, player2 wins!")
+elif player_1=="R" and player_2=="S":
+  print("Rock smashes Scissors, player1 wins!")
+  
